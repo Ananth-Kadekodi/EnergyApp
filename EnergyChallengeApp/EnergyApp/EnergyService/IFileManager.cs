145111d9b070aeb7cf4beb2c60b93b0d3ab2c594
Ananth Kadekodi <ananthkadekodi@gmail.com>
2022-07-21 16:04:31 +0800

    Implemented interface

---
+++ EnergyChallengeApp/EnergyApp/EnergyService/IFileManager.cs
@@ -0,0 +1,12 @@
+﻿using EnergyApp.DataModels;
+
+namespace EnergyApp.EnergyService
+{
+    public interface IFileManager
+    {
+        GenerationData LoadInputXMLFile(string fileName, string inputDataFilePath);
+        ReferenceData LoadReferenceDataFile(string fileName, string referenceDataFilePath);
+        string RetrieveFilePath(string fileDirectory, string fileName);
+        void WriteOutputToFile(GenerationOutput generationOutputData, string outFileName, string inputFileName, string outputDataFilePath, string inputDataFilePath, string archiveDataFilePath);
+    }
+}
